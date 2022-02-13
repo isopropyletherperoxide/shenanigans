aa0e8ccffdb6967a52cd010c77d698430d8e34d4
Aryl Halide <endless.sine@gmail.com>
2022-02-13 23:24:39 +0200

    finished picker

--- picker.c
+++ picker.c
@@ -1,40 +1,16 @@
-/* program that picks things to do for me 
-   doubles as a coin flip */
-#define STR_LNGTH 5
-#define STR_AMNT 1
-
+// coinflip or wheel of names program its pretty nice
 #include <stdio.h>
 #include <time.h>
 
-int string_write(int);
-int srand (int);
+void srand(int);
 int rand();
-char item[STR_AMNT][STR_LNGTH];
 
 
-int main(void)
-{
-	int ch;
-        int idx;
-        srand(time(NULL));
-	int r = rand() % STR_AMNT;
-	for (idx = 0; idx <= STR_AMNT; idx++)
-	{
-	  string_write(idx);
+int main(int argc, char *argv[]){
+	if (argc < 2){
+		printf("add arguments, stupid\n"); // degradation kink
+		return 1;
 	}
-	idx = rand() % (STR_AMNT + 1);
-	for (ch = 0; ch <= STR_LNGTH - 1; ch++)
-	{
-	printf("%c", item[idx][ch]);
-	}
-}
-
-int string_write(int c)
-{
-	int i;
-	for (i = 0; i <= STR_LNGTH; i++)
-	{
-		item[c][i] = getchar();
-	}
-	return 0;
+	srand(time(0));
+	printf("%s \n", argv[(rand() % (argc - 1)) + 1 ]);
 }
