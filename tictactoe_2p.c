6dcebdb71d09a7672e24c6472814e891e5ae438c
Aryl Halide <endless.sine@gmail.com>
2022-05-29 00:30:44 +0300

    MEOW

--- tictactoe_2p.c
+++ tictactoe_2p.c
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <stdio.h>
 #include <curses.h>
 
 char grid[3][3]; // tictactoe grid, first index is row, second is column
